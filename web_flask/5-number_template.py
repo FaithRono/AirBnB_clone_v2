3e622c8d0b1b5a07e60ce6f5a40687378c1a2574
faith rono <faithrono132@gmail.com>
2024-03-22 12:07:46 -0700

    updates

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -37,5 +37,12 @@ def imanumber(n):
     """display “n is a number” only if n is an integer"""
     return "{:d} is a number".format(n)
 
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def numbersandtemplates(n):
+    """display a HTML page only if n is an integer"""
+    return render_template('5-number.html', n=n)
+
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
